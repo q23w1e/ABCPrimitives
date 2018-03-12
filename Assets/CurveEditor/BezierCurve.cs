5a0bc9db09bea3cd3b5ddcd5b0d60a2cf4c3d172
Oleg Patseev <o_patseev@wargaming.net>
2018-03-12 11:26:16 +0300

    implementing bezier cubic

--- Assets/CurveEditor/BezierCurve.cs
+++ Assets/CurveEditor/BezierCurve.cs
@@ -7,18 +7,31 @@ public class BezierCurve : MonoBehaviour
 	public Vector3[] points = new Vector3[] {
 		new Vector3(-0.5f, 0f, 0f),
 		new Vector3(0, 0f, 0.5f),
-		new Vector3(0.5f, 0f, 0f)
+		new Vector3(0.5f, 0f, -0.5f),
+		new Vector3(1.0f, 0f, 0f),
 	};
 
-	// Use this for initialization
-	void Start () 
+	Vector3[] _transformedPoints = new Vector3[3];
+
+	// each time the whole array will be recalcualted=|
+	public Vector3[] TransformedPoints
 	{
-		
+		get 
+		{
+			for (int i = 0; i < points.Length; i++)
+				_transformedPoints[i] = this.transform.TransformPoint(points[i]);
+
+			return _transformedPoints;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () 
+
+	public Vector3 getPoint(float t)
 	{
-		
+		return this.transform.TransformPoint(BezierCubic.getPoint(points[0], points[1], points[2], points[3], t));
+	}
+
+	public Vector3 getVelocity(float t)
+	{
+		return Vector3.zero;
 	}
 }
--- Assets/CurveEditor/Editor/BezierCurveInspector.cs
+++ Assets/CurveEditor/Editor/BezierCurveInspector.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(BezierCurve))]
 public class BezierCurveInspector : Editor 
 {
+    public const int lineSegments = 10;
 
     BezierCurve curve;
     Transform handleTransform;
@@ -15,31 +17,57 @@ public class BezierCurveInspector : Editor
         handleTransform = curve.transform;
         handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;        
 
-        drawPointHandle(0);
-        drawPointHandle(1);
-        drawPointHandle(2);
+        Vector3 p0 = drawPointHandle(0);
+        Vector3 p1 = drawPointHandle(1);
+        Vector3 p2 = drawPointHandle(2);
+        Vector3 p3 = drawPointHandle(3);
 
-        drawPointsLines();
+        drawPointsLines(p0, p1, p2, p3);
+        drawBezierCurve(p0, p1, p2, p3);
     }
-    
-    void drawPointsLines()
-    {
+
+    void drawPointsLines(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    {   
+        float screenSpaceLineSize = 0.1f; 
         Handles.color = Color.gray;
-        int[] segmentIndices = new int[] {0, 1, 1, 2};
-        Vector3[] transformedPoints = new Vector3[curve.points.Length];
-        
-        Handles.DrawDottedLines(curve.points, segmentIndices, 0.5f);
+        Handles.DrawDottedLine(p0, p1, screenSpaceLineSize);
+        Handles.DrawDottedLine(p1, p2, screenSpaceLineSize);
+        Handles.DrawDottedLine(p2, p3, screenSpaceLineSize);
     }
 
-    void drawPointHandle(int index)
+    void drawBezierCurve(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
     {
-        Vector3 p = handleTransform.TransformPoint(curve.points[index]);
+        float t = 1f / lineSegments;
+        Vector3 tail = BezierCubic.getPoint(p0, p1, p2, p3, 0f);
+        for (int i = 1; i <= lineSegments; i++)
+        {
+            Handles.color = Color.red;
+            Vector3 head = BezierCubic.getPoint(p0, p1, p2, p3, t * i);
+            Handles.DrawLine(tail, head);
+            tail = head;
+        }
+    }
+
+    Vector3 getVelocity(Vector3 p0,Vector3 p1,Vector3 p2, float t)
+    {
+        return handleTransform.TransformPoint(BezierQuadratic.getTrangent(p0, p1, p2, t) - p0);
+    }
+
+    void drawVelocityDirection(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+
+    }
+
+    Vector3 drawPointHandle(int index)
+    {
+        Vector3 point = handleTransform.TransformPoint(curve.points[index]);
         EditorGUI.BeginChangeCheck();
-            p = Handles.PositionHandle(p, handleRotation);
+            point = Handles.PositionHandle(point, handleRotation);
         if (EditorGUI.EndChangeCheck())
             Undo.RecordObject(curve, "Move Point");
             EditorUtility.SetDirty(curve);
-            curve.points[index] = handleTransform.InverseTransformPoint(p);
-        // drawPointsLines();
+            curve.points[index] = handleTransform.InverseTransformPoint(point);
+
+        return point;
     }
 }
