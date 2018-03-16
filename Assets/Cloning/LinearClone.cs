8bcc1d5d8560176a533c807ecea7d8a3374beb22
Oleg Patseev <o_patseev@wargaming.net>
2018-03-16 19:10:08 +0300

    implementing linear clone tool

---
+++ Assets/Cloning/LinearClone.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearClone: MonoBehaviour
+{
+    public GameObject prefab;
+   
+    int _count = 1;
+    Vector3 _end = 4f * Vector3.right;
+    Stack<GameObject> clones = new Stack<GameObject>{};
+
+    public Vector3 Tail
+    {
+        get { return this.transform.position; }
+    }
+    public Vector3 Head
+    {
+        get { return this.transform.TransformPoint(_end); }
+        set 
+        {
+            if (this.transform.InverseTransformPoint(value) != _end) 
+            {
+                _end = this.transform.InverseTransformPoint(value);
+                recalculatePositions(); 
+            }
+        }
+    }
+
+    public int Count
+    {
+        get { return _count; }
+        set 
+        { 
+            if ( _count == value) { return; };
+            
+            _count = Mathf.Max(1, value);
+        }
+    }
+
+    public void updateClonesList()
+    {
+        if (!prefab) 
+        {
+            Debug.LogWarning("Source prefab is empty");
+            return;
+        }
+
+        int diff = Count - clones.Count;
+        while (diff != 0)
+        {
+            if (diff > 0)
+            {
+                diff--;
+                GameObject clone = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+                clones.Push(clone);
+            }
+            else
+            {
+                diff++;
+                DestroyImmediate(clones.Pop());
+            }
+        }
+    }
+
+    public void recalculatePositions()
+    {
+        int n = (clones.Count - 1) == 0 ? 1 : (clones.Count - 1);
+        Vector3 d = (Head - Tail) / n;
+        int i = 0;
+        foreach (var clone in clones)
+        {
+            if (clone) { clone.transform.position = this.transform.position + (d * i); } 
+            i++;
+        }
+    }
+
+    public void Clear()
+    {
+        foreach (var clone in clones)
+        {
+            if (clone) DestroyImmediate(clone);
+            
+        }
+        clones.Clear();
+        Count = 0;
+    }
+}
